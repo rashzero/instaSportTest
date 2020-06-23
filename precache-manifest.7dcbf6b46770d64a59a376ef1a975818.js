f1f1fd63bf8e31a9224a78ab4a81f820da0330ce
rashzero <rashzero0@gmail.com>
2020-06-23 19:56:55 +0300

    Updates

---
+++ precache-manifest.7dcbf6b46770d64a59a376ef1a975818.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7cd6130d7a87ec2d81d6488ef83c0d4",
+    "url": "/instaSportTest/index.html"
+  },
+  {
+    "revision": "cb241f8bb59e5b8eaf54",
+    "url": "/instaSportTest/static/css/main.84facf53.chunk.css"
+  },
+  {
+    "revision": "f2782be654345c1baa3f",
+    "url": "/instaSportTest/static/js/2.01d4888e.chunk.js"
+  },
+  {
+    "revision": "cb241f8bb59e5b8eaf54",
+    "url": "/instaSportTest/static/js/main.1351d206.chunk.js"
+  },
+  {
+    "revision": "7c0c2ec0bdcd4a16dcce",
+    "url": "/instaSportTest/static/js/runtime~main.a81a6564.js"
+  }
+]);
